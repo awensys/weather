a5ac828061640f7d49cf64866dbc99288cf94059
awensys <40073807+awensys@users.noreply.github.com>
2018-06-20 09:24:11 +0300

    add main fragment

---
+++ app/src/main/java/com/example/denis/weather/MainFragment.java
@@ -0,0 +1,34 @@
+package com.example.denis.weather;
+
+import com.example.denis.weather.R;
+import com.example.denis.weather.BR;
+import com.stfalcon.androidmvvmhelper.mvvm.fragments.BindingFragment;
+import com.example.denis.weather.databinding.FragmentMainBinding;
+
+
+public class MainFragment extends BindingFragment<MainFragmentVM, FragmentMainBinding> {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    protected MainFragmentVM onCreateViewModel(FragmentMainBinding binding) {
+        return new MainFragmentVM(this);
+    }
+
+    @Override
+    public int getVariable() {
+        return BR.viewModel;
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_main;
+    }
+
+}
---
+++ app/src/main/java/com/example/denis/weather/MainFragmentVM.java
@@ -0,0 +1,11 @@
+package com.example.denis.weather;
+
+import com.stfalcon.androidmvvmhelper.mvvm.fragments.FragmentViewModel;
+
+public class MainFragmentVM extends FragmentViewModel<MainFragment> {
+
+    public MainFragmentVM(MainFragment fragment) {
+        super(fragment);
+    }
+
+}
